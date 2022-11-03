aff011ba911b273f105b3ec772bcce020e1c652e
Christy Man <manchristy83@yahoo.de>
2022-11-03 09:13:14 +0100

    Submit exercise but func3 not working

--- exercise-03/functions.py
+++ exercise-03/functions.py
@@ -9,6 +9,20 @@ def func1(a, b):
     return None
   return type(a)
 
+def func2(*kwargs):
+  if len(kwargs) == 2:
+    a, b = kwargs
+    return func1(a, b)
+  else :
+    return len(kwargs)
+
+def func3(*kwargs):
+  if kwargs[0] == "a" and kwargs[1] == "b":
+    return True
+
+print(func2(3,3))
+# print(func3(a = 1, b = 2))
+
 func1(1,2)
 func1("Welt", "Hallo")
 func1(None, None)
--- exercise-03/listcomprehension.py
+++ exercise-03/listcomprehension.py
@@ -1,3 +1,8 @@
 x = [0,1,2,3,4,5,6,7,8,9]
 y = [True, False, False, True, "False", False, True, True, True, "True"]
 
+a = [2*num for num in x]
+b = [num for num in x if num % 2 == 0]
+c = [num for num in x if num]
+d = [s for s in y if type(s) == str]
+e = [[True]*el for el in x if el % 2 == 1]
