33e5a9e4edfc8145d0ece0c48b1fd99e59686c28
Hsarit <Hannah.saritzoglou@gmx.de>
2022-11-03 13:13:06 +0100

    done e2

--- exercise-02/calc.py
+++ exercise-02/calc.py
@@ -2,5 +2,10 @@ print("Enter number 1: ")
 num1 = int(input())
 print("Enter number 2: ")
 num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+print("Do you want to use addition or multiplication?")
+i = str(input())
+if "add" in i:
+    print("Sum: " + str(num1+num2))
+if "multi" in i:
+    print("Product: " + str(num1*num2))
+
