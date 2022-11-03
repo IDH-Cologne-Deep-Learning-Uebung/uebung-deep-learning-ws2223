a86ef034c5cb301677af2199fed04ed635a24d63
lixux01 <rex.lixux@outlook.de>
2022-11-03 13:25:25 +0100

    fertig

--- exercise-02/calc.py
+++ exercise-02/calc.py
@@ -1,6 +1,18 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+def calc():
+    print("Enter number 1: ")
+    num1 = int(input())
+    print("Enter number 2: ")
+    num2 = int(input())
+    print("Choose addition (add) or multiplication (multi)")
+    operation = str(input())
+    if operation in ["add","addition","Add","Addition"]:
+        print("Sum: " + str(num1+num2))
+    if operation in ["multi","multiplication","Multi","Multiplication"]:
+        print("Product: " + str(num1*num2))
+    print("Do this again?")
+    answer = str(input())
+    if answer in ["y", "Y", "yes", "Yes", "YES"]:
+        calc()
+    else:
+        return print("Thank you")
+calc()
