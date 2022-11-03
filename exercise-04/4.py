b00c2108af846a5770599801193637d432fa592d
Dwennimmen <amini_l2001@yahoo.de>
2022-11-03 20:38:41 +0100

    Homework done

--- exercise-04/4.py
+++ exercise-04/4.py
@@ -3,9 +3,25 @@ import random
 l1 = [[int(random.normalvariate(50,20)) for x in range(random.randint(5,20))] for x in range(10)]
 
 i = 0
-for outerlist in l1:
+for row in l1:
   print(i, end=" ")
-  for element in outerlist:
+  for element in row:
     print(element, end=" ")
   print()
   i = i + 1
+
+print("\nWith Padding: \n")
+rows = 0
+
+
+for row in l1:
+  print(rows, end=" ")
+  for index in range(20):
+    try:
+      row[index] # throws IndexError
+    except IndexError:
+      row.append(0)
+  for element in row:
+      print(element, end=" ")
+  print()
+  rows = rows + 1
