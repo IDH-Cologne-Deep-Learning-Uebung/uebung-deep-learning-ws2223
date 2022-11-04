d09cc9c60f6ba494fb4b4c24614b2617ba4d8766
TheOfficialSand <lmarkic@smail.uni-koeln.de>
2022-11-04 12:37:17 +0100

    Padding task completed.

--- exercise-04/4.py
+++ exercise-04/4.py
@@ -1,11 +1,12 @@
 import random
 
-l1 = [[int(random.normalvariate(50,20)) for x in range(random.randint(5,20))] for x in range(10)]
+l1 = [[int(random.normalvariate(50, 20)) for x in range(random.randint(5, 20))] for y in range(10)]
 
 i = 0
 for outerlist in l1:
-  print(i, end=" ")
-  for element in outerlist:
-    print(element, end=" ")
-  print()
-  i = i + 1
+    print(i, end=" ")
+    outerlist += [0 for i in range(20-len(outerlist))]
+    for element in outerlist:
+        print(element, end=(" " * (3-len(str(element)))))
+    print()
+    i = i + 1
