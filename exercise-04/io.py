afc10b5209a551e1f824c1678a77872d0d005d46
Cecilia Lauffer <cecilia.lauffer@gmail.com>
2022-11-08 08:38:18 +0100

    HA fertig

--- exercise-04/io.py
+++ exercise-04/io.py
@@ -1,4 +1,4 @@
-wi = open("wiki.txt", mode="r", encoding="UTF-8").readlines()
-open("short.txt", mode="w", encoding="UTF-8").writelines([line for line in wi if len(line) < 30])
-open("april.txt", mode="w", encoding="UTF-8").writelines(line for line in wi if "April" in line)
-open("articles.txt", mode="w", encoding="UTF-8").writelines([line for line in wi if line[0:4] in ["Der ", "Die ", "Das "]])
+w = open("wiki.txt", mode="r", encoding="UTF-8").readlines()
+open("short.txt", mode="w", encoding="UTF-8").writelines([line for line in w if len(line) < 30])
+open("april.txt", mode="w", encoding="UTF-8").writelines(line for line in w if "April" in line)
+open("articles.txt", mode="w", encoding="UTF-8").writelines([line for line in w if line[0:4] in ["Der ", "Die ", "Das "]])
