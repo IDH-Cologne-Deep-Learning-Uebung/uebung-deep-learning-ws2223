f00cd4ed988d880589b895db74d4d31746d37ee8
SHerding97 <simonherding@aol.com>
2022-11-09 14:07:10 +0100

    Done

--- exercise-04/4.py
+++ exercise-04/4.py
@@ -5,6 +5,12 @@ l1 = [[int(random.normalvariate(50,20)) for x in range(random.randint(5,20))] fo
 i = 0
 for outerlist in l1:
   print(i, end=" ")
+  for elementPosition in range(20):
+    try:
+        if(outerlist[elementPosition] != -1):
+            pass
+    except:
+        outerlist.append(0)
   for element in outerlist:
     print(element, end=" ")
   print()
