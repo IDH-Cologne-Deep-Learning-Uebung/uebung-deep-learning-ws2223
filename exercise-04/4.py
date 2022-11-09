a0d049492e18320d78374ab7ef21ef319d10466b
dr123v <dr123v@gmail.com>
2022-11-09 14:56:06 +0100

    finished

--- exercise-04/4.py
+++ exercise-04/4.py
@@ -4,8 +4,15 @@ l1 = [[int(random.normalvariate(50,20)) for x in range(random.randint(5,20))] fo
 
 i = 0
 for outerlist in l1:
+  
   print(i, end=" ")
+  j=0
   for element in outerlist:
     print(element, end=" ")
+    j+=1
+  while j<20:
+    print(0,end = " ")
+    j=j+1
   print()
   i = i + 1
+ 
