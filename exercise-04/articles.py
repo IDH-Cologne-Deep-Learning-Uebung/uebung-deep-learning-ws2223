0d8633133081b454865883acdce91289a949496d
dr123v <dr123v@gmail.com>
2022-11-09 14:44:25 +0100

    io.py finished, sadly I get file descriptor err

--- exercise-04/articles.py
+++
@@ -1,8 +0,0 @@
-data = open("wiki.txt",mode ="r",encoding="UTF-8")
-
-A = []
-articles=("Der","Die","Das")
-
-[A.append(line) for line in data if line.startswith(articles) and  line.startswith(" ",3,4)]
-
-data.close()
--- exercise-04/io.py
+++ exercise-04/io.py
@@ -1,6 +1,19 @@
 data = open("wiki.txt",mode ="r",encoding="UTF-8")
 
-A = []
-[A.append(line) for line in data if len(line)<30]
+kurz = open("short.txt",mode ="w")
+[kurz.write(line) for line in data if len(line)<30]
+kurz.close()
+
+
+articles=("Der","Die","Das")
+Artikel = open("articles.txt",mode ="w")
+[Artikel.write(line) for line in data if line.startswith(articles) and  line.startswith(" ",3,4)]
+Artikel.close()
+
+
+april = open("april.txt",mode = "w")
+[april.write(line) for line in data if "April" in line]
+april.close()
+
 
 data.close()
