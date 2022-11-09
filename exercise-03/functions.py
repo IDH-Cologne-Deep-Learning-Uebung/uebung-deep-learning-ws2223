cda39880d8d5cc57db7e5ea51c92ee366b5a3c7b
lixux01 <rex.lixux@outlook.de>
2022-11-09 23:19:58 +0100

    fertig

--- exercise-03/functions.py
+++ exercise-03/functions.py
@@ -14,3 +14,24 @@ func1("Welt", "Hallo")
 func1(None, None)
 func1(11, "Freunde")
 func1(5.4, 3.6)
+
+def func2( *Variablen ):
+  n = len(Variablen)
+  if n < 2:
+    return str(n)
+  elif n == 2:
+    return func1(Variablen)
+  else:
+    return n
+
+def func3( **Variablen_benannt ):
+  if 'a' in Variablen_benannt and 'b' in Variablen_benannt:
+    return func1(Variablen_benannt.get('a'),Variablen_benannt.get('b'))
+  else:
+    return func2(Variablen_benannt)
+
+print(func2("Hallo", 2, 5.5))
+
+print(func3(a = 1, b = 5))
+
+print(func3(a = "Hello"))
--- exercise-03/listcomprehension.py
+++ exercise-03/listcomprehension.py
@@ -1,3 +1,17 @@
 x = [0,1,2,3,4,5,6,7,8,9]
 y = [True, False, False, True, "False", False, True, True, True, "True"]
 
+a = [n*2 for n in x]
+print(a)
+
+b = [gerade for gerade in x if gerade % 2 == 0]
+print(b)
+
+c = [wahr for wahr in y if wahr == True]
+print(c)
+
+d = [text for text in y if isinstance(text,str) == True]
+print(d)
+
+e = [[True] * ungerade for ungerade in x if ungerade % 2 == 1]
+print(e)
