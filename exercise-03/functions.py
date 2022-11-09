b0315fbd80c0678b003f363ba968d961b5b3400a
laryrauh <laryrauh@localhost>
2022-11-09 22:56:47 +0100

    commit test

--- exercise-03/functions.py
+++ exercise-03/functions.py
@@ -9,8 +9,28 @@ def func1(a, b):
     return None
   return type(a)
 
-func1(1,2)
-func1("Welt", "Hallo")
-func1(None, None)
-func1(11, "Freunde")
-func1(5.4, 3.6)
+print(func1(1,2))
+print(func1("Welt", "Hallo"))
+print(func1(None, None))
+print(func1(11, "Freunde"))
+print(func1(5.4, 3.6))
+
+def func2(*args):
+    if len(args) < 2:
+        return args
+    if len(args) == 2:
+        return func1(*args)
+    if len(args) > 2:
+        return len(args)
+
+print(func2("Hello"))
+print(func2(1,3))
+print(func2(1,2,3,4,5,6))
+
+def func3(**kwargs):
+    if 'a' in kwargs and 'b' in kwargs:
+        return func1(a, b) # scheitere hier daran, was ich func1 übergeben soll. trage ich kwargs ein, wird das als type an func1 übergeben
+    else:
+        return func2(*kwargs)
+    
+print (func3(a=1, b=10, c=7, d=5))
