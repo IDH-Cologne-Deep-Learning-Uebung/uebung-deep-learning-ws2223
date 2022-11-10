72784758d75abe38fb3b8529ad2de5c938ff3df6
dr123v <dr123v@gmail.com>
2022-11-10 13:39:50 +0100

    neuer test

--- exercise-04/io.py
+++ exercise-04/io.py
@@ -1,18 +1,18 @@
-#data = open("wiki.txt",mode ="r",encoding="UTF-8")
-with open("wiki.txt",mode ="r",encoding="ANSI") as data:
-    #kurz = open("short.txt",mode ="w")
-    with open("short.txt",mode ="w",encoding="ANSI") as kurz:
-        [kurz.write(line) for line in data if len(line)<30]
+data = open("wiki.txt",mode ="r",encoding="utf-8")
+#with open("wiki.txt",mode ="r",encoding="ANSI") as data:
+kurz = open("short.txt",mode ="w",encoding = "utf-8")
+#    with open("short.txt",mode ="w",encoding="ANSI") as kurz:
+[kurz.write(line) for line in data if len(line)<30]
+
+articles=("Der","Die","Das")
+artikel = open("articles.txt",mode ="w",encoding="utf-8")
+
+#    with open("articles.txt",mode ="w",encoding="ANSI") as artikel:
+[artikel.write(line) for line in data if line.startswith(articles) and  line.startswith(" ",3,4)]
 
 
-    articles=("Der","Die","Das")
-    #artikel = open("articles.txt",mode ="w")
+april = open("april.txt",mode = "w",encoding="utf-8")
+#    with open("april.txt",mode = "w",encoding="ANSI") as april:
+[april.write(line) for line in data if "April" in line]
 
-    with open("articles.txt",mode ="w",encoding="ANSI") as artikel:
-        [artikel.write(line) for line in data if line.startswith(articles) and  line.startswith(" ",3,4)]
-
-
-    #april = open("april.txt",mode = "w")
-    with open("april.txt",mode = "w",encoding="ANSI") as april:
-        [april.write(line) for line in data if "April" in line]
-
+data.close
