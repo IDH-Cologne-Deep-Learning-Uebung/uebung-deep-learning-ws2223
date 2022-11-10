c37e08f7fbd18d9119bae7955c7c392763c5ad83
dr123v <dr123v@gmail.com>
2022-11-10 13:24:10 +0100

    test uwu

--- exercise-04/io.py
+++ exercise-04/io.py
@@ -1,19 +1,18 @@
-data = open("wiki.txt",mode ="r",encoding="UTF-8")
-
-kurz = open("short.txt",mode ="w")
-[kurz.write(line) for line in data if len(line)<30]
-kurz.close()
-
-
-articles=("Der","Die","Das")
-artikel = open("articles.txt",mode ="w")
-[artikel.write(line) for line in data if line.startswith(articles) and  line.startswith(" ",3,4)]
-artikel.close()
+#data = open("wiki.txt",mode ="r",encoding="UTF-8")
+with open("wiki.txt",mode ="r",encoding="utf-8") as data:
+    #kurz = open("short.txt",mode ="w")
+    with open("short.txt",mode ="w",encoding="utf-8") as kurz:
+        [kurz.write(line) for line in data if len(line)<30]
 
 
-april = open("april.txt",mode = "w")
-[april.write(line) for line in data if "April" in line]
-april.close()
+    articles=("Der","Die","Das")
+    #artikel = open("articles.txt",mode ="w")
+
+    with open("articles.txt",mode ="w",encoding="utf-8") as artikel:
+        [artikel.write(line) for line in data if line.startswith(articles) and  line.startswith(" ",3,4)]
 
 
-data.close()
+    #april = open("april.txt",mode = "w")
+    with open("april.txt",mode = "w",encoding="utf-8") as april:
+        [april.write(line) for line in data if "April" in line]
+
