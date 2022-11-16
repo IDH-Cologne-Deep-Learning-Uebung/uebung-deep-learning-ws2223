ca60e89df0ef3d266634b3eacdde1005411d8519
Hsarit <Hannah.saritzoglou@gmx.de>
2022-11-16 13:45:25 +0100

    done

--- exercise-04/4.py
+++ exercise-04/4.py
@@ -3,9 +3,21 @@ import random
 l1 = [[int(random.normalvariate(50,20)) for x in range(random.randint(5,20))] for x in range(10)]
 
 i = 0
-for outerlist in l1:
-  print(i, end=" ")
-  for element in outerlist:
-    print(element, end=" ")
-  print()
-  i = i + 1
+for outerline in l1:
+    z = len(outerline)
+    try:
+        l = outerline[19]
+    except:
+        z = len(outerline)
+        outerline.append(0)
+        while z < 19:
+            z= len(outerline)
+            outerline.append(0)
+
+for outerline in l1:
+    print(i, end=" ")
+    for element in outerline:
+        print(element, end=" ")
+    print()
+    i = i +1
+
