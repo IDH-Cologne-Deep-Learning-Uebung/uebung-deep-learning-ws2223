24aec4efff66e2333253f28f28ed0113da1d585c
abudanca1 <abudanca@smail.uni-koeln.de>
2022-11-17 01:39:33 +0100

    Update io.py

--- exercise-04/io.py
+++ exercise-04/io.py
@@ -1,12 +1,12 @@
-open("wiki.txt") as f:
+open("wiki.txt", encoding="utf-8") as f:
 wikiText = f.readlines()
 rep = [rep.replace("\n" + "") for rep in wikiText]    
 
-open("short.txt", "w") as f:
+open("short.txt", encoding="utf-8", "w") as f:
 [f.write(rep + "\n") for rep in rep (if len(rep) < 30)]
 
-open("articles.text", "w") as f:
+open("articles.text", encoding="utf-8", "w") as f:
 [f.write(rep + '\n') for rep in rep if rep.startswith('Der' or 'Das' or 'Die') ]
  
-open("april.txt", "w") as f:
+open("april.txt", encoding="utf-8", "w") as f:
 [f.write(rep + '\n') for rep in rep if 'April' in rep]
