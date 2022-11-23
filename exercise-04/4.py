aa6c9b5364bf60e2ab35fdb1ec1d3bc27bff98a0
lixux01 <rex.lixux@outlook.de>
2022-11-23 15:24:38 +0100

    message

--- exercise-04/4.py
+++ exercise-04/4.py
@@ -5,7 +5,11 @@ l1 = [[int(random.normalvariate(50,20)) for x in range(random.randint(5,20))] fo
 i = 0
 for outerlist in l1:
   print(i, end=" ")
+  j = 0
   for element in outerlist:
     print(element, end=" ")
+    j = j + 1
+  for n in range(j,20):
+      print(0, end=" ")
   print()
   i = i + 1
