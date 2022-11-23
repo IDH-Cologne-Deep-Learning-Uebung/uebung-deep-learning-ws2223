71db12e385fd27d5560ad83e562ae17b499d7502
whummel <wolf-andre_hummel@live.de>
2022-11-23 22:08:34 +0100

    Finished exercise 4.

--- exercise-05/titanic.py
+++ exercise-05/titanic.py
@@ -4,8 +4,6 @@ import pandas as pd
 df = pd.read_csv("data/train.csv")
 
 df = df.drop(columns=["Name", "PassengerId"])
-df = df.dropna()
-print(df.columns)
 # print(df["Sex"].unique())
 df["Sex"] = df["Sex"].map({'male': 0, 'female': 1})
 unique_cabin = df["Cabin"].unique()
@@ -14,11 +12,34 @@ cabin_numeric = dict(zip(unique_cabin, range(len(unique_cabin))))
 df["Cabin"] = df["Cabin"].map(cabin_numeric)
 
 ticket_numeric = dict(zip(df["Ticket"].unique(), range(len(df["Ticket"].unique()))))
-print()
+df["Ticket"] = df["Ticket"].map(ticket_numeric)
+
+# One expression
+df["Embarked"] = df["Embarked"].map(
+    dict(zip(
+        df["Embarked"].unique(),
+        range(len(df["Embarked"].unique()))
+    ))
+)
+df = df.dropna()
+
 # ## Step 3
 # 1. Remove the columns "Name" and "PassengerId" (because we know they are irrelevant for our problem).
 # 2. Convert all non-numeric columns into numeric ones. The non-numeric columns are "Sex", "Cabin", "Ticket" and "Embarked".
 # 3. Remove all rows that contain missing values
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+Y_col = 'Survived'
+X_cols = df.loc[:, df.columns != Y_col].columns
+X_Train, X_test, y_train, y_test = train_test_split(
+    df[X_cols], df[Y_col], 
+    test_size=0.20, random_state=66
+    )
+
+model = LogisticRegression().fit(X_Train, y_train)
+accuracy = model.score(X_test, y_test)
+
+print(accuracy)
 
 
 # ## Step 4
