271d94fd80d5719ef4227a3cc90eaba4c8c6a99c
xsandyx <103143981+xsandyx@users.noreply.github.com>
2022-11-25 08:18:59 +0100

    exercise completed

--- exercise-05/titanic.py
+++ exercise-05/titanic.py
@@ -1,49 +1,70 @@
 import pandas as pd
-import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import recall_score
-from sklearn.metrics import precision_score
-from sklearn.metrics import f1_score
 
 # read the data from a CSV file (included in the repository)
 df = pd.read_csv("data/train.csv")
-df.drop(labels=['PassengerId', 'Name'], axis=1, inplace=True)
-
-df['Sex'] = df['Sex'].replace(['male', 'female'], '1', '0')
-df['Embarked'] = df['Embarked'].replace(['Q', 'C', 'S'], '0', '1', '2')
-df['Cabin'] = df['Cabin'].replace(['B', 'C', 'D', 'E', 'F',' G'], '0', '1', '2', '3', '4', '5')
-
-df["Sex"] = pd.to_numeric(df["Sex"])
-df["Embarked"] = pd.to_numeric(df["Embarked"])
-df["Cabin"] = pd.to_numeric(df["Cabin"])
-
-x_data = df[['PcClass', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']]
-y_data = df['Survived']
-
-df_train = df.sample(frac=0.8, random_state=1)
-df_test = df.drop(df_train.index)
-
-regressor = LinearRegression()
-regressor.fit(x_data, y_data)
-
-y_pred = regressor.predict(y_data)
-
-recall_score(y_data, y_pred, *['PcClass', 'Sex', 'Age','SibSp' ,'Parch' ,'Ticket' ,'Ticket' ,'Fare' ,'Cabin' ,'Embarked'])
-precision_score(y_data, y_pred, *['PcClass', 'Sex','Age','SibSp','Parch','Ticket','Ticket','Fare','Cabin','Embarked'])
-f1_score(y_data, y_pred, *['PcClass','Sex','Age','SibSp','Parch','Ticket','Ticket','Fare','Cabin','Embarked'])
-
-
-
 
 # ## Step 3
 # 1. Remove the columns "Name" and "PassengerId" (because we know they are irrelevant for our problem).
-# 2. Convert all non-numeric columns into numeric ones. The non-numeric columns are "Sex", "Cabin", "Ticket" and "Embarked".
-# 3. Remove all rows that contain missing values
+df = df.drop("Name", 1)
+df = df.drop("PassengerId", 1)
 
+
+# 2. Convert all non-numeric columns into numeric ones. The non-numeric columns are "Sex", "Cabin", "Ticket" and "Embarked".
+def make_numeric(df):
+    df["Sex"] = pd.factorize(df["Sex"])[0]
+    df["Cabin"] = pd.factorize(df["Cabin"])[0]
+    df["Ticket"] = pd.factorize(df["Ticket"])[0]
+    df["Embarked"] = pd.factorize(df["Embarked"])[0]
+    return df
+
+
+df = make_numeric(df)
+
+# 3. Remove all rows that contain missing values
+df = df.dropna()
 
 # ## Step 4
 # 1. As a next step, we need to split the input features from the training labels. This can be done easily with `pandas`.
-# 2. Secondly, we need to split training and test data. This can be done with the function [`sklearn.model_selection.train_test_split()`](https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html#sklearn.model_selection.train_test_split) from the `scikit-learn` library.
-# 3. Finally, initialize a LogisticRegression object with a `liblinear` solver, and fit it to the training data.
-# 4. Lastly, calculate precision/recall/f-score on the test data using the appropriate functions from `scikit-learn`.
+y = df["Survived"]
+x = df.drop("Survived", 1)
 
+# 2. Secondly, we need to split training and test data. This can be done with the function [`sklearn.model_selection.train_test_split()`](https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html#sklearn.model_selection.train_test_split) from the `scikit-learn` library.
+
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=0, test_size=0.1)
+
+# 3. Finally, initialize a LogisticRegression object with a `liblinear` solver, and fit it to the training data.
+from sklearn.linear_model import LogisticRegression
+
+classifier = LogisticRegression(random_state=0, solver="liblinear")
+classifier.fit(x_train, y_train)
+
+# 4. Lastly, calculate precision/recall/f-score on the test data using the appropriate functions from `scikit-learn`.
+y_pred = classifier.predict(x_test)
+
+from sklearn.metrics import precision_score, recall_score, f1_score
+
+print("precision: " + str(precision_score(y_test, y_pred)))
+print("recall: " + str(recall_score(y_test, y_pred)))
+print("f1: " + str(f1_score(y_test, y_pred)))
+Footer
+© 2022
+GitHub, Inc.
+Footer
+navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact
+GitHub
+Pricing
+API
+Training
+Blog
+About
+uebung - deep - learning - ws2223 / titanic_ref_solution.py
+at
+master · IDH - Cologne - Deep - Learning - Uebung / uebung - deep - learning - ws2223
