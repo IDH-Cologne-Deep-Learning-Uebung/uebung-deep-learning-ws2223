21a0fbe2c94ae529e9730adb25af349043180683
Hsarit <Hannah.saritzoglou@gmx.de>
2022-11-29 16:40:21 +0100

    done

--- exercise-05/titanic.py
+++ exercise-05/titanic.py
@@ -1,18 +1,51 @@
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import precision_score, recall_score, f1_score
+
 
 # read the data from a CSV file (included in the repository)
 df = pd.read_csv("data/train.csv")
+print(df)
+
+# ##Step 3
+# #1.
+#df.drop("Name", inplace=True, axis=1)
+del df["Name"]
+del df["PassengerId"]
+
+# #2. (first replaced single things, so they could be converted to number
+#df["Sex"] = df["Sex"].astype(int)
+df["Sex"] = df["Sex"].replace({"male": "0", "female": "1"})
+df["Ticket"] = df["Ticket"].replace("[^0-9]", "", regex=True)
+df["Cabin"] = df["Cabin"].replace({"A": "1.0", "B": "2.0", "C": "3.0", "D": "4.0", "E": "5.0", "F": "6.0", "G": "7.0", "T": "8"}, regex=True)
+df["Embarked"] = df["Embarked"].replace({"C": "1", "Q": "2", "S": "3"}, regex=True)
+df["Sex"] = pd.to_numeric(df["Sex"], errors="coerce")
+df["Cabin"] = pd.to_numeric(df["Cabin"], errors="coerce")
+df["Ticket"] = pd.to_numeric(df["Ticket"], errors="coerce")
+df["Embarked"] = pd.to_numeric(df["Embarked"], errors="coerce")
+
+# #3.
+df = df.dropna()
 
 
-# ## Step 3
-# 1. Remove the columns "Name" and "PassengerId" (because we know they are irrelevant for our problem).
-# 2. Convert all non-numeric columns into numeric ones. The non-numeric columns are "Sex", "Cabin", "Ticket" and "Embarked".
-# 3. Remove all rows that contain missing values
+
+# ##Step 4
+# #1.
+y = np.array(df["Survived"])
+X = np.array(df.drop(["Survived"], inplace=True, axis=1))
+
+# #2.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+# #3.
+classifier = LogisticRegression(random_state=0, solver="liblinear")
+classifier.fit(X_train, y_train)
 
 
-# ## Step 4
-# 1. As a next step, we need to split the input features from the training labels. This can be done easily with `pandas`.
-# 2. Secondly, we need to split training and test data. This can be done with the function [`sklearn.model_selection.train_test_split()`](https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html#sklearn.model_selection.train_test_split) from the `scikit-learn` library.
-# 3. Finally, initialize a LogisticRegression object with a `liblinear` solver, and fit it to the training data.
-# 4. Lastly, calculate precision/recall/f-score on the test data using the appropriate functions from `scikit-learn`.
-
+# #4.
+y_pred = classifier.predict(x_test)
+print("precision: "+ str(precision_score(y_test, y_pred)))
+print("recall: "+ str(recall_score(y_test, y_pred)))
+print("f1: "+ str(f1_score(y_test, y_pred)))
