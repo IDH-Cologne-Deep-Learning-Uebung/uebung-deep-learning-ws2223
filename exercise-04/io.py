0834cfdb305e8f482dccd06393b0fdb562463810
Jacek Jackowski <jacekjackowski@Jans-MacBook-Air.local>
2022-11-30 12:35:32 +0100

    exercise 6 finished (i hope)

--- exercise-04/io.py
+++ exercise-04/io.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 textfile = open("wiki.txt", "r")
 shortfile = open("short.txt", "w")
 articlefile = open("articles.txt", "w")
@@ -16,3 +17,35 @@ articlefile.close()
 aprillist = [line for line in content if "April" in line]
 aprilfile.writelines(aprillist)
 aprilfile.close()
+=======
+# Because we will need this code a few times, 
+# we define a function for it
+def ListToFile(List, Filename):
+  # open file for writing
+  file = open(Filename, "w")
+  
+  # iterate over list
+  for Line in List:
+    # write list into file
+    file.write(Line)
+
+  # close file
+  file.close()
+
+# open file for reading
+file = open("wiki.txt")
+
+# get a list of all sentences
+sentences = file.readlines()
+
+# close file
+file.close()
+
+# call function for different subsets of sentences
+ListToFile([x for x in sentences if len(x) < 30], 
+  "short.txt")
+ListToFile([x for x in sentences if x.startswith("Der") or x.startswith("Die") or x.startswith("Das")], 
+  "articles.txt")
+ListToFile([x for x in sentences if "April" in x], 
+  "april.txt")
+>>>>>>> master
