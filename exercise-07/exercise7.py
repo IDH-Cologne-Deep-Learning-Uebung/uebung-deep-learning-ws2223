a2a2bddefce6600f1179dba05421bb3ade99c772
Wolf-Andre H <wolf-andre_hummel@live.de>
2022-12-07 18:19:01 +0100

    Added baseline model.

---
+++ exercise-07/exercise7.py
@@ -0,0 +1,38 @@
+import bz2
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from keras import Sequential
+
+
+def get_labels_and_texts(file, n=12000):
+    labels = []
+    texts = []
+    i = 0
+    for line in bz2.BZ2File(file):
+        x = line.decode("utf-8")
+        labels.append(int(x[9]) - 1)
+        texts.append(x[10:].strip())
+        i = i + 1
+        if i >= n:
+          return np.array(labels), texts
+    return np.array(labels), texts
+
+labels, texts = get_labels_and_texts('train.ft.txt.bz2')
+test_lbls, test_texts = get_labels_and_texts('test.ft.txt.bz2')
+
+vectorizer = CountVectorizer()
+
+vectorizer.fit(texts)
+
+X_train = vectorizer.transform(texts)
+X_test = vectorizer.transform(texts)
+
+classifier = LogisticRegression()
+classifier.fit(X_train, labels)
+score = classifier.score(X_test, test_lbls)
+print("Accuracy:", score)
+
+# model = Sequential()
+# model.add(layers.Embedding(len(texts)))
