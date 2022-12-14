ac679ea830b871208c72c70e45f37bca5d31ba9a
TheOfficialSand <103219272+TheOfficialSand@users.noreply.github.com>
2022-12-14 20:51:56 +0100

    yay it works

--- exercise-08/exercise.py
+++ exercise-08/exercise.py
@@ -2,10 +2,10 @@ import bz2
 
 import numpy as np
 
-from tensorflow.python.keras import models, layers
-from tensorflow.keras.preprocessing.text import Tokenizer, text_to_word_sequence
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras import regularizers
+from keras import models, layers
+from keras.preprocessing.text import Tokenizer, text_to_word_sequence
+from keras.utils import pad_sequences
+from keras import regularizers
 
 
 def get_labels_and_texts(file, n=10000):
@@ -21,23 +21,47 @@ def get_labels_and_texts(file, n=10000):
           return np.array(labels), texts
     return np.array(labels), texts
 
+print("Running...")
+MAX_LENGTH = 5
 
 train_labels, train_texts = get_labels_and_texts('data/train.ft.txt.bz2')
-
+print("Training data retrieved.")
+test_labels, test_texts = get_labels_and_texts('data/test.ft.txt.bz2')
+print("Test data retrieved.")
+tokenizer = Tokenizer()
+print("Tokenizer initialized.")
+train_sequences = tokenizer.texts_to_sequences(train_texts)
+train_array = pad_sequences(train_sequences, maxlen=MAX_LENGTH, padding="post")
+print("Training data prepared.")
+test_sequences = tokenizer.texts_to_sequences(test_texts)
+print("Test data prepared.")
+test_array = pad_sequences(test_sequences, maxlen=MAX_LENGTH, padding="post")
 
 ffnn = models.Sequential()
-ffnn.add(layers.Input(shape=(MAX_LENGTH)))
+print("Model initialized.")
+ffnn.add(layers.Input(shape=(MAX_LENGTH,)))
+print("Input layer added.")
+ffnn.add(layers.Embedding(MAX_LENGTH, embeddings_regularizer=regularizers.L2(0.01), output_dim=100))
+ffnn.add(layers.Flatten())
+print("Embedding layer added.")
 ffnn.add(layers.Dense(100, activation="sigmoid"))
 ffnn.add(layers.Dropout(0.5))
+print("Dense layer added.")
 ffnn.add(layers.Dense(50, activation="sigmoid"))
 ffnn.add(layers.Dropout(0.5))
+print("Dense layer added.")
 ffnn.add(layers.Dense(1, activation="sigmoid"))
+print("Output layer added.")
 
+print("\nSummary:")
 ffnn.summary()
+print()
 
 ffnn.compile(loss="binary_crossentropy", optimizer="sgd",
   metrics=["accuracy"])
+print("Model compiled.")
 
-ffnn.fit(train_texts, train_labels, epochs=10, batch_size=10, verbose=1)
+ffnn.fit(train_array, train_labels, epochs=10, batch_size=10, verbose=1)
+print("Model trained.")
 
-
+ffnn.evaluate(test_array, test_labels)
