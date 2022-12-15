11f97076061f95f7bb08509228fb39998c128f1d
dmuell59 <dmuell59@smail.uni-koeln.de>
2022-12-15 01:07:40 +0100

    Aufgabe 8 fertig

--- exercise-08/exercise.py
+++ exercise-08/exercise.py
@@ -5,10 +5,8 @@ import numpy as np
 from tensorflow.python.keras import models, layers
 from tensorflow.keras.preprocessing.text import Tokenizer, text_to_word_sequence
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras import regularizers
 
-
-def get_labels_and_texts(file, n=10000):
+def get_labels_and_texts(file, n=2000000):
     labels = []
     texts = []
     i = 0
@@ -24,6 +22,14 @@ def get_labels_and_texts(file, n=10000):
 
 train_labels, train_texts = get_labels_and_texts('data/train.ft.txt.bz2')
 
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(train_texts)
+vocab_size = len(tokenizer.word_index) + 1
+train_texts = tokenizer . texts_to_sequences(train_texts)
+
+MAX_LENGTH = max(len(train_ex)for train_ex in train_texts)
+
+train_texts = pad_sequences(train_texts, maxlen=MAX_LENGTH, padding="post")
 
 ffnn = models.Sequential()
 ffnn.add(layers.Input(shape=(MAX_LENGTH)))
@@ -33,11 +39,10 @@ ffnn.add(layers.Dense(50, activation="sigmoid"))
 ffnn.add(layers.Dropout(0.5))
 ffnn.add(layers.Dense(1, activation="sigmoid"))
 
+print("Network created")
+
 ffnn.summary()
 
-ffnn.compile(loss="binary_crossentropy", optimizer="sgd",
-  metrics=["accuracy"])
+ffnn.compile(loss="binary_crossentropy", optimizer="sgd", metrics=["accuracy"])
 
 ffnn.fit(train_texts, train_labels, epochs=10, batch_size=10, verbose=1)
-
-
