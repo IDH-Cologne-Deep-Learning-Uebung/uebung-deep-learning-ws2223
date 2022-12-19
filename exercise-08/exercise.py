051d5fb3b09c1ee215593e4beb86eedb6efb2ac1
Steffi S.-E <steffix317@gmail.com>
2022-12-19 13:09:03 +0100

    nothing works to increase the accuracy

--- exercise-08/exercise.py
+++ exercise-08/exercise.py
@@ -2,10 +2,10 @@ import bz2
 
 import numpy as np
 
-from tensorflow.python.keras import models, layers
-from tensorflow.keras.preprocessing.text import Tokenizer, text_to_word_sequence
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras import regularizers
+from keras import models, layers
+from keras.preprocessing.text import Tokenizer, text_to_word_sequence
+from keras import regularizers
+from keras.utils import pad_sequences
 
 
 def get_labels_and_texts(file, n=10000):
@@ -18,15 +18,35 @@ def get_labels_and_texts(file, n=10000):
         texts.append(x[10:].strip())
         i = i + 1
         if i >= n:
-          return np.array(labels), texts
+            return np.array(labels), texts
     return np.array(labels), texts
 
 
 train_labels, train_texts = get_labels_and_texts('data/train.ft.txt.bz2')
 
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(train_texts)
+vocab_size = len(tokenizer.word_index) + 1
+train_texts = tokenizer.texts_to_sequences(train_texts)
+
+MAX_LENGTH = max(len(train_ex) for train_ex in train_texts)
+
+train_texts = pad_sequences(train_texts, maxlen=MAX_LENGTH, padding="post")
+
+model = models.Sequential()
+model.add(layers.Input(shape=MAX_LENGTH))
+model.add(layers.Embedding(vocab_size, 200, input_length=MAX_LENGTH))
+model.add(layers.Flatten())
+model.add(layers.Dense(100, activation="sigmoid"))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(50, activation="sigmoid"))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(1, activation="sigmoid"))
+
+model.summary()
 
 ffnn = models.Sequential()
-ffnn.add(layers.Input(shape=(MAX_LENGTH)))
+ffnn.add(layers.Input(shape=MAX_LENGTH))
 ffnn.add(layers.Dense(100, activation="sigmoid"))
 ffnn.add(layers.Dropout(0.5))
 ffnn.add(layers.Dense(50, activation="sigmoid"))
@@ -36,8 +56,6 @@ ffnn.add(layers.Dense(1, activation="sigmoid"))
 ffnn.summary()
 
 ffnn.compile(loss="binary_crossentropy", optimizer="sgd",
-  metrics=["accuracy"])
+             metrics=["accuracy"])
 
 ffnn.fit(train_texts, train_labels, epochs=10, batch_size=10, verbose=1)
-
-
