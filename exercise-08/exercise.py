ed343e05487d9e0784970e2620e63ee29aab154d
laryrauh <laryrauh@aol.com>
2022-12-22 10:39:49 +0100

    exercise 8

--- exercise-08/exercise.py
+++ exercise-08/exercise.py
@@ -5,10 +5,9 @@ import numpy as np
 from tensorflow.python.keras import models, layers
 from tensorflow.keras.preprocessing.text import Tokenizer, text_to_word_sequence
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras import regularizers
 
 
-def get_labels_and_texts(file, n=10000):
+def get_labels_and_texts(file, n=2000000):
     labels = []
     texts = []
     i = 0
@@ -24,6 +23,14 @@ def get_labels_and_texts(file, n=10000):
 
 train_labels, train_texts = get_labels_and_texts('data/train.ft.txt.bz2')
 
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(train_texts)
+vocab_size = len(tokenizer.word_index) + 1
+train_texts = tokenizer . texts_to_sequences(train_texts)
+
+MAX_LENGTH = max(len(train_ex)for train_ex in train_texts)
+
+train_texts = pad_sequences(train_texts, maxlen=MAX_LENGTH, padding="post")
 
 ffnn = models.Sequential()
 ffnn.add(layers.Input(shape=(MAX_LENGTH)))
@@ -32,6 +39,8 @@ ffnn.add(layers.Dropout(0.5))
 ffnn.add(layers.Dense(50, activation="sigmoid"))
 ffnn.add(layers.Dropout(0.5))
 ffnn.add(layers.Dense(1, activation="sigmoid"))
+
+print("Network created")
 
 ffnn.summary()
 
